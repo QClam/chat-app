5c5dbf8d819d1407fc94ce1fb2b129009f118884
Xuan Quy <hoangxuanquy5@gmail.com>
2025-02-19 18:32:21 +0700

    Setup Project, User List UI Design

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+
+import './chatList.css'
+
+const ChatList = () => {
+
+const [addMore, setAddMore] = useState(false);
+
+  return (
+    <div className='chatList'>
+        <div className="search">
+            <div className="searchBar">
+                <img src="./search.png" alt="" />
+                <input type="text" placeholder='Tìm người dùng' />
+            </div>
+            <img src={addMore ? "./minus.png" : "./plus.png"} alt="" className='add' onClick={() => setAddMore((prev) => !prev)}/>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatList
