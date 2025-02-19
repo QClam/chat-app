966257215df7452ac2251d6b90cdc9862e71d0ad
Xuan Quy <hoangxuanquy5@gmail.com>
2025-02-19 21:32:48 +0700

    Chat UI

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import './chat.css'
+import EmojiPicker from "emoji-picker-react";
+import "./chat.css";
 
 const Chat = () => {
-  return (
-    <div className='chat'>Chat</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
 
-export default Chat
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  console.log(text);
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              delectus impedit inventore quia corrupti sit beatae esse cumque
+              perferendis. Ratione doloremque, quo commodi autem alias aut
+              deserunt eos assumenda ipsum.
+            </p>
+            <span>1 phút trước</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <img src="./avatar.png" alt="" /> */}
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              delectus impedit inventore quia corrupti sit beatae esse cumque
+              perferendis. Ratione doloremque, quo commodi autem alias aut
+              deserunt eos assumenda ipsum.
+            </p>
+            <span>1 phút trước</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              delectus impedit inventore quia corrupti sit beatae esse cumque
+              perferendis. Ratione doloremque, quo commodi autem alias aut
+              deserunt eos assumenda ipsum.
+            </p>
+            <span>1 phút trước</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <img src="./avatar.png" alt="" /> */}
+          <div className="texts">
+            <img src="https://cdn.oneesports.vn/cdn-data/sites/4/2024/12/honkai-star-rail-castorice.jpg" alt="" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              delectus impedit inventore quia corrupti sit beatae esse cumque
+              perferendis. Ratione doloremque, quo commodi autem alias aut
+              deserunt eos assumenda ipsum.
+            </p>
+            <span>1 phút trước</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Nhập tin nhắn..."
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Gửi</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -43,6 +43,34 @@ const [addMore, setAddMore] = useState(false);
                 <p>Hello</p>
             </div>
         </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+                <span>Jane Doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
     </div>
   )
 }
