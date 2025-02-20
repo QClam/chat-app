6974242241ee4476014dd651d59bbe54c824858d
Xuan Quy <hoangxuanquy5@gmail.com>
2025-02-20 08:16:06 +0700

    Detail User

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import EmojiPicker from "emoji-picker-react";
 import "./chat.css";
@@ -6,6 +6,12 @@ import "./chat.css";
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  })
 
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
@@ -80,6 +86,7 @@ const Chat = () => {
             <span>1 phút trước</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
       <div className="bottom">
         <div className="icons">
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
 
-import './detail.css'
+import "./detail.css";
 
 const Detail = () => {
-  return (
-    <div className='detail'>Detail</div>
-  )
-}
+    return (
+        <div className="detail">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Lorem ipsum, dolor sit amet.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Cài đặt</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
 
-export default Detail
+
+                <div className="option">
+                    <div className="title">
+                        <span>Quyền riêng tư và hỗ trợ</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>Ảnh</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn.oneesports.vn/cdn-data/sites/4/2024/12/honkai-star-rail-castorice.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn.oneesports.vn/cdn-data/sites/4/2024/12/honkai-star-rail-castorice.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn.oneesports.vn/cdn-data/sites/4/2024/12/honkai-star-rail-castorice.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn.oneesports.vn/cdn-data/sites/4/2024/12/honkai-star-rail-castorice.jpg" alt="" />
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="option">
+                    <div className="title">
+                        <span>File được chia sẻ</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Chặn</button>
+                <button className="logout">Đăng xuất</button>
+            </div>
+        </div>
+    );
+};
+
+export default Detail;
